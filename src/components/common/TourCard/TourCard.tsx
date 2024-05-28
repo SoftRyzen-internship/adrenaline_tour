50da3513c9cfbe4aa5e7cc926b83aeb48e6dd6b3
Daryna Karmazin <daryna.karmazin@gmail.com>
2024-05-28 14:54:32 +0300

    Added responsive cover img

--- src/components/common/TourCard/TourCard.tsx
+++ src/components/common/TourCard/TourCard.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 import { ITourCardProps } from './TourCard.types';
 
@@ -6,14 +7,22 @@ const TourCard: React.FC<ITourCardProps> = ({ data }) => {
   const { imgSrc, alt, title, date, location, type, duration, link } = data;
 
   return (
-    <div>
-      <Image src={imgSrc} width={328} height={291} alt={alt} />
-      <h2>{title}</h2>
-      <p>{date}</p>
-      <p>{location}</p>
-      <p>{type}</p>
-      <p>{duration}</p>
-      <a href={link}>More details</a>
+    <div className='relative flex h-[441px] w-[328px] flex-col border border-red md:h-[439px] md:w-[334px] xl:h-[485px] xl:w-[384px]'>
+      <p className='absolute right-4 top-4 bg-white px-2 py-[7.5px]'>{date}</p>
+      <div className='relative h-[291px] w-full xl:h-[333px]'>
+        <Image src={imgSrc} layout='fill' objectFit='cover' alt={alt} />
+      </div>
+      <div className='flex-grow pt-2'>
+        <h2>{title}</h2>
+        <p>{location}</p>
+        <p>{type}</p>
+        <p>{duration}</p>
+      </div>
+      <div className='mb-1'>
+        <Link className='mt-auto text-accentDefaultOrange' href={link}>
+          Детальніше -&gt;
+        </Link>
+      </div>
     </div>
   );
 };
