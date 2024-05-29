490ecc217c0a8a07ab2815a183bcf141119dd812
DisRachik <radchenkoig@gmail.com>
2024-05-29 15:19:54 +0300

    fix grammar into tour page

--- src/app/(main)/tours/[id]/page.tsx
+++ src/app/(main)/tours/[id]/page.tsx
@@ -5,9 +5,7 @@ export interface ISingleTourPageProps {
 const SingleTourPage: React.FC<ISingleTourPageProps> = ({ params }) => {
   return (
     <>
-      <h1 className=' bg-green-400 text-6xl'>
-        `Single Tour Page - ${params.id}`
-      </h1>
+      <h1 className=' bg-green-400 text-6xl'>Single Tour Page - {params.id}</h1>
     </>
   );
 };
