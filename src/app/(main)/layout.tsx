75b3139949a709c7d30a86f0578ade23d4dbf456
DisRachik <radchenkoig@gmail.com>
2024-05-29 10:47:54 +0300

    add basic routing

---
+++ src/app/(main)/layout.tsx
@@ -0,0 +1,10 @@
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      <>{children}</>
+      {/* <h2 className='bg-blue-300'>Here will be Contact section with form</h2> */}
+    </>
+  );
+};
+
+export default Layout;
