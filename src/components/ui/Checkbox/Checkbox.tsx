534fee781164426cb026aa6a170812f554c0d322
Daryna Karmazin <daryna.karmazin@gmail.com>
2024-05-30 12:37:30 +0300

    Added checked state to checkbox

--- src/components/ui/Checkbox/Checkbox.tsx
+++ src/components/ui/Checkbox/Checkbox.tsx
@@ -1,10 +1,17 @@
+import clsx from 'clsx';
+
+import s from './Checkbox.module.css';
 import { CheckboxProps } from './Checkbox.types';
 
 const Checkbox = ({ name, label }: CheckboxProps) => {
   return (
     <label className='flex gap-3 text-light font-light text-white md:text-medium'>
-      <input type='checkbox' name={name} />
-      {label}
+      <input
+        className={clsx('', s['custom-checkbox'])}
+        type='checkbox'
+        name={name}
+      />
+      <p>{label}</p>
     </label>
   );
 };
