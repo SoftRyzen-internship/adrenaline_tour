e73947b2d71901d0bff3708dad50eef1ee0eb8d4
Oleg <ikslprint@gmail.com>
2024-05-31 16:23:44 +0300

    add mobile style

--- src/components/common/Disclosure/Disclosure.tsx
+++ src/components/common/Disclosure/Disclosure.tsx
@@ -8,10 +8,13 @@ import {
 
 import MinusIcon from '@/../public/icons/minus.svg';
 import PlusIcon from '@/../public/icons/plus.svg';
+import { faq } from '@/data';
 
 interface IDisclosureProps {}
 
 const DisclosureMain: React.FC<IDisclosureProps> = () => {
+  const { disclosures } = faq;
+
   const [activeDisclosurePanel, setActiveDisclosurePanel] = useState(null);
 
   const handleToggle = newPanel => {
@@ -27,81 +30,15 @@ const DisclosureMain: React.FC<IDisclosureProps> = () => {
     });
   };
 
-  const accordions = [
-    {
-      id: 1,
-      label: 'Що брати?',
-      items: [
-        {
-          id: 1.1,
-          question: 'Що саме брати в похід?',
-          answer: [
-            {
-              title: 'Взуття',
-              text: [
-                'Ботінки на високій шнурівці з фіксацією голіностопного суглоба.',
-                'Запасне взуття (кросівки або кеди) як варіант для табору, а також для переходів через струмки та на великі відстані.',
-              ],
-            },
-            { title: 'Одяг', text: ['Пункт 1', 'Пункт 2', 'Пункт 3'] },
-            {
-              title: 'Аксесуари',
-              text: ['Пункт 1', 'Пункт 2', 'Пункт 3', 'Пункт 4', 'Пункт 5'],
-            },
-          ],
-        },
-        {
-          id: 1.2,
-          question:
-            'Чи можу я взяти з собою камеру або дрон для зйомки пригоди?',
-          answer:
-            'Так, ви можете взяти з собою камеру або дрон. Однак, для безпеки та з поваги до приватності інших, існують вказівки щодо їх використання.',
-        },
-        {
-          id: 1.3,
-          question: 'Що брати на сплав, каноїнг або рафтінг?',
-          answer: 'Водостійка куртка, дощовик',
-        },
-      ],
-    },
-    {
-      id: 2,
-      label: 'Добре знати',
-      items: [
-        {
-          id: 2.1,
-          question:
-            'Чи можуть учасники-початківці брати участь у цих активностях?',
-          answer: 'Another answer.',
-        },
-        {
-          id: 2.2,
-          question: 'Наскільки фізично вимогливі ці активності?',
-          answer: 'Yet another answer.',
-        },
-        {
-          id: 2.3,
-          question: 'Наскільки фізично вимогливі ці активності?',
-          answer: 'Yet another answer.',
-        },
-        {
-          id: 2.4,
-          question: 'Наскільки фізично вимогливі ці активності?',
-          answer: 'Yet another answer.',
-        },
-      ],
-    },
-  ];
-
   return (
     <div className='mx-auto w-full max-w-lg'>
       <ul>
-        {accordions.map(accordion => (
-          <li key={accordion.id} className=''>
-            <p className='inline-block rotate-3 border-2 border-accentDefaultOrange px-4 py-3 font-unbounded text-accentDefaultOrange'>
-              {accordion.label}
+        {disclosures.map(disclosure => (
+          <li key={disclosure.id} className='mb-8'>
+            <p className='mb-6 inline-block rotate-3 border-2 border-accentDefaultOrange px-4 py-3 font-unbounded text-accentDefaultOrange'>
+              {disclosure.label}
             </p>
-            {accordion.items.map(item => (
+            {disclosure.items.map(item => (
               <Disclosure
                 as='div'
                 className='border-b-[1px] border-blue32 py-6'
@@ -111,12 +48,12 @@ const DisclosureMain: React.FC<IDisclosureProps> = () => {
                 {({ open, close }) => (
                   <>
                     <DisclosureButton
-                      className='group flex w-full items-center justify-between text-left'
+                      className='group flex w-full items-center justify-between  text-left'
                       onClick={() => {
                         handleToggle({ key: item.id, open, close });
                       }}
                     >
-                      <span className=' max-w-[272px] font-inter text-lg/6 font-bold text-blueDefault'>
+                      <span className='max-w-[272px] font-inter text-lg/6 font-bold text-blueDefault'>
                         {item.question}
                       </span>
                       {open ? (
@@ -133,21 +70,25 @@ const DisclosureMain: React.FC<IDisclosureProps> = () => {
                         />
                       )}
                     </DisclosureButton>
-                    <DisclosurePanel className='p-l-4 mt-2 line-clamp-4 pl-4 font-inter text-sm font-medium text-dark'>
-                      {typeof item.answer === 'string' ? (
-                        <p className='border-l-[1px] border-blue32 pl-4'>
-                          {item.answer}
+                    <DisclosurePanel className='line-clamp-4  pl-4 pt-[42px]  font-inter text-sm font-medium text-dark'>
+                      {typeof item.answers === 'string' ? (
+                        <p className='border-l-[1px] border-blue32 pl-4 before:absolute before:left-1 before:top-1 before:mr-0.5 before:block before:size-2 before:rounded-full before:bg-dark'>
+                          {item.answers}
                         </p>
                       ) : (
-                        item.answer.map((description, index) => (
+                        item.answers.map((answer, index) => (
                           <div
                             key={index}
                             className=' border-l-[1px] border-blue32 pl-4'
                           >
-                            <p className='font-bold'>{description.title}</p>
-                            <ul className=''>
-                              {description.text.map((point, index) => (
-                                <li key={index}>* {point}</li>
+                            <p className='font-bold'>{answer.title}</p>
+                            <ul>
+                              {answer.text.map((point, index) => (
+                                <li key={index} className='relative pl-5'>
+                                  <span className='before:absolute before:left-1 before:top-2  before:block before:size-[6px] before:rounded-full before:bg-dark'>
+                                    {point}
+                                  </span>
+                                </li>
                               ))}
                             </ul>
                           </div>
