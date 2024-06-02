fe59cedcdaa73c1ba94008bacb842a8b6be9a8ba
Oleg <ikslprint@gmail.com>
2024-06-02 12:48:56 +0300

    add desktop styles for disclosure

--- src/components/common/Disclosure/Disclosure.tsx
+++ src/components/common/Disclosure/Disclosure.tsx
@@ -48,115 +48,114 @@ const DisclosureMain: React.FC<IDisclosureProps> = () => {
   };
 
   return (
-    <div className='mx-auto w-full'>
+    <div className='mx-auto w-full '>
       <ul>
         {disclosures.map(disclosure => (
-          <li key={disclosure.id} className='mb-8'>
-            <p className='mb-6 inline-block rotate-3 border-2 border-accentDefaultOrange px-4 py-3 font-unbounded text-xs/[1.3] font-bold  text-accentDefaultOrange md:text-sm/[1.3]'>
+          <li
+            key={disclosure.id}
+            className='mb-8  xl:flex  xl:justify-between '
+          >
+            <p className='mb-6 inline-block rotate-3 border-2 border-accentDefaultOrange px-4 py-3 font-unbounded text-xs/[1.3] font-bold text-accentDefaultOrange  md:text-base/[1.3] md:text-sm/[1.3] xl:self-baseline'>
               {disclosure.label}
             </p>
-            {disclosure.items.map(item => (
-              <Disclosure
-                as='div'
-                className='border-b-[1px] border-blue32 py-6'
-                key={item.id}
-                defaultOpen={item.id === 1.1}
-              >
-                {({ open, close }) => (
-                  <>
-                    <DisclosureButton
-                      className='group flex w-full items-center justify-between  text-left'
-                      onClick={() => {
-                        if (!open) {
-                          // On the first click, the panel is opened but the "open" prop's value is still false. Therefore the falsey verification
-                          // This will make so the panel close itself when we click it while open
-                          close();
-                        }
-                        handleToggle({ key: item.id, open, close });
-                      }}
-                    >
-                      <span className='max-w-[272px] font-inter text-lg/[1.3] font-bold text-blueDefault md:text-xl/[1.3]'>
-                        {item.question}
-                      </span>
-                      {open ? (
-                        <MinusIcon
-                          width={24}
-                          height={24}
-                          className='size-6 stroke-accentDefaultOrange'
-                        />
-                      ) : (
-                        <PlusIcon
-                          width={24}
-                          height={24}
-                          className='size-6 stroke-accentDefaultOrange'
-                        />
-                      )}
-                    </DisclosureButton>
-                    <DisclosurePanel
-                      ref={textRef}
-                      className='pl-4 pt-[42px]  font-inter text-sm/[1.3] font-medium text-dark  md:text-base/[1.4]'
-                    >
-                      {item.answers.map((answer, index) => (
-                        <div
-                          key={index}
-                          className='border-l-[1px] border-blue32 pl-4'
-                        >
-                          {answer.title && (
-                            <p className='mb-2 font-bold'>{answer.title}</p>
-                          )}
+            <div>
+              {disclosure.items.map(item => (
+                <Disclosure
+                  as='div'
+                  className='border-b-[1px] border-blue32 py-6 xl:w-[801px]'
+                  key={item.id}
+                  defaultOpen={item.id === 1.1}
+                >
+                  {({ open, close }) => (
+                    <>
+                      <DisclosureButton
+                        className='group flex w-full items-center justify-between  text-left'
+                        onClick={() => {
+                          if (!open) {
+                            // On the first click, the panel is opened but the "open" prop's value is still false. Therefore the falsey verification
+                            // This will make so the panel close itself when we click it while open
+                            close();
+                          }
+                          handleToggle({ key: item.id, open, close });
+                        }}
+                      >
+                        <span className='max-w-[272px] font-inter text-lg/[1.3] font-bold text-blueDefault md:max-w-[644px] md:text-xl/[1.3] xl:max-w-[745px] xl:text-2xl/[1.1]'>
+                          {item.question}
+                        </span>
+                        {open ? (
+                          <MinusIcon
+                            width={24}
+                            height={24}
+                            className='size-6 stroke-accentDefaultOrange'
+                          />
+                        ) : (
+                          <PlusIcon
+                            width={24}
+                            height={24}
+                            className='size-6 stroke-accentDefaultOrange'
+                          />
+                        )}
+                      </DisclosureButton>
+                      <DisclosurePanel
+                        ref={textRef}
+                        className='pl-4 pt-[42px]  font-inter text-sm/[1.3] font-medium text-dark  md:text-base/[1.4] xl:text-lg/[1.5]'
+                      >
+                        {item.answers.map((answer, index) => (
+                          <div
+                            key={index}
+                            className='border-l-[1px] border-blue32 pl-4'
+                          >
+                            {answer.title && (
+                              <p className='mb-2 font-bold'>{answer.title}</p>
+                            )}
 
-                          <ul className='mb-2'>
-                            {answer.text.map((point, index) => (
-                              <>
-                                <li
-                                  key={index}
-                                  className={clsx(
-                                    'relative',
-                                    item.icon && 'pl-5',
-                                  )}
-                                >
-                                  <span
+                            <ul className='mb-2'>
+                              {answer.text.map((point, index) => (
+                                <>
+                                  <li
+                                    key={index}
                                     className={clsx(
-                                      item.icon &&
-                                        'before:absolute before:left-1 before:top-2  before:block before:size-[6px] before:rounded-full before:bg-dark',
+                                      'relative',
+                                      item.icon && 'pl-5',
                                     )}
                                   >
-                                    {point}
-                                  </span>
-                                </li>
-                                {item.margin && (
-                                  <p className='block h-4 text-transparent'>
-                                    Пустий рядок
-                                  </p>
-                                )}
-                              </>
-                            ))}
-                          </ul>
+                                    <span
+                                      className={clsx(
+                                        item.icon &&
+                                          'before:absolute before:left-1 before:top-2  before:block before:size-[6px] before:rounded-full before:bg-dark',
+                                      )}
+                                    >
+                                      {point}
+                                    </span>
+                                  </li>
+                                  {item.margin && (
+                                    <p className='block h-4 text-transparent'>
+                                      Пустий рядок
+                                    </p>
+                                  )}
+                                </>
+                              ))}
+                            </ul>
+                          </div>
+                        ))}
+                      </DisclosurePanel>
+                      {showReadMoreButton && open && (
+                        <div className='pl-4'>
+                          <div className='border-l-[1px] border-blue32 pl-4'>
+                            <Button variant='readMore-secondary' type='button'>
+                              Читати далі
+                            </Button>
+                          </div>
                         </div>
-                      ))}
-                    </DisclosurePanel>
-                    {showReadMoreButton && open && (
-                      <div className='pl-4'>
-                        <div className='border-l-[1px] border-blue32 pl-4'>
-                          <Button variant='readMore-secondary' type='button'>
-                            Читати далі
-                          </Button>
-                        </div>
-                      </div>
-                    )}
-                  </>
-                )}
-              </Disclosure>
-            ))}
+                      )}
+                    </>
+                  )}
+                </Disclosure>
+              ))}
+            </div>
           </li>
         ))}
       </ul>
-      <div>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
-        commodo cursus magna, vel scelerisque nisl consectetur et. &nbsp;
-        <p className='block h-4 text-transparent'></p> {/* Пустая строка */}
-        Sed posuere consectetur est at lobortis.
-      </div>
     </div>
   );
 };
