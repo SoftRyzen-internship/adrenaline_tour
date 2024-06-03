6dc8b31b7d5dc908819986bbf5e26e14fcd5d042
Daryna Karmazin <daryna.karmazin@gmail.com>
2024-06-03 17:23:51 +0300

    Added bg for section

--- src/sections/Contacts/Contact.tsx
+++ src/sections/Contacts/Contact.tsx
@@ -1,11 +1,21 @@
+import clsx from 'clsx';
+
 import { IdForScroll } from '@/@types';
 import Form from '@/components/common/Form';
 import Phones from '@/components/ui/Phones';
 import { form } from '@/data';
 
+import s from './Contacts.module.css';
+
 const Contact = () => {
   return (
-    <section id={IdForScroll.CONTACTS} className='section bg-darkBlue'>
+    <section
+      id={IdForScroll.CONTACTS}
+      className={clsx(
+        s['contacts-bg'],
+        'section bg-darkBlue pt-[124px] md:pt-[139px] xl:pt-[122px]',
+      )}
+    >
       <div className='container'>
         <h2 className='section-title mb-6 text-white smOnly:pl-[10px] md:mb-10 xl:mb-20 xl:max-w-[854px]'>
           {form.title}
