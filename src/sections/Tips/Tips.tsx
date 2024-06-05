d7d06c48b5df9eb531259c0fb0e4e790cf3827b8
Daryna Karmazin <daryna.karmazin@gmail.com>
2024-06-05 10:48:34 +0300

    Added section title

--- src/sections/Tips/Tips.tsx
+++ src/sections/Tips/Tips.tsx
@@ -3,8 +3,13 @@ import { tipsData } from '@/data';
 
 const Tips = () => {
   return (
-    <section className='section'>
+    <section className='section bg-[#F5FAFD]'>
       <div className='container'>
+        <h2 className='section-title mb-8 text-blueDefault md:mb-12 xl:mb-16'>
+          {tipsData.title[0]}
+          <br />
+          {tipsData.title[1]}
+        </h2>
         <ul className='flex flex-col gap-3 md:gap-4 xl:flex-row xl:items-center xl:justify-between xl:gap-4'>
           {tipsData.cards.map(({ id, title, text }) => {
             return <CardTips key={id} title={title} text={text} />;
