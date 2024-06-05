dfd8e4330cbeb0d8da551f2c42585e03d1a13656
Oleg <ikslprint@gmail.com>
2024-06-05 10:18:35 +0300

    add start files

---
+++ src/sections/Faq/Faq.tsx
@@ -0,0 +1,11 @@
+const Faq: React.FC = () => {
+  return (
+    <section className='section'>
+      <div className='container'>
+        <h2 className='section-title'>FAQ</h2>
+      </div>
+    </section>
+  );
+};
+
+export default Faq;
