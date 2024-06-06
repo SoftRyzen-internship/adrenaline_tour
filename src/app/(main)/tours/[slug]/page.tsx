bbef3bbbdaced900688fb4e5b25dd09ad17b7054
Daryna Karmazin <daryna.karmazin@gmail.com>
2024-06-06 13:14:30 +0300

    Updated ISingleTourPageProps

--- src/app/(main)/tours/[slug]/page.tsx
+++ src/app/(main)/tours/[slug]/page.tsx
@@ -1,9 +1,8 @@
+import { Metadata } from 'next';
+
 export interface ISingleTourPageProps {
   params: { id: string };
-  searchParams: { [key: string]: string | string[] | undefined };
 }
-
-import { Metadata } from 'next';
 
 const BASE_APP_URL = process.env.BASE_APP_URL as string;
 
@@ -11,14 +10,18 @@ export async function generateMetadata({
   params,
 }: ISingleTourPageProps): Promise<Metadata> {
   const id = params.id;
-  const tour = await fetch(`${BASE_APP_URL}tours/${id}`).then(res =>
-    res.json(),
-  );
+  const response = await fetch(`${BASE_APP_URL}tours/${id}`);
+
+  if (!response.ok) {
+    throw new Error(`Failed to fetch tour data: ${response.statusText}`);
+  }
+
+  const tour = await response.json();
 
   return {
     title: tour.title,
     alternates: {
-      canonical: params.id,
+      canonical: `${BASE_APP_URL}tours/${id}`,
     },
   };
 }
@@ -26,7 +29,7 @@ export async function generateMetadata({
 const SingleTourPage: React.FC<ISingleTourPageProps> = ({ params }) => {
   return (
     <>
-      <h1 className=' bg-green-400 text-6xl'>Single Tour Page - {params.id}</h1>
+      <h1 className='bg-green-400 text-6xl'>Single Tour Page - {params.id}</h1>
     </>
   );
 };
