2ce2f7be9ffb409ca7bb53521b23b2f00d1d6376
Daryna Karmazin <daryna.karmazin@gmail.com>
2024-06-06 09:07:44 +0300

    Added Tips section to the page

--- src/app/(main)/tours/page.tsx
+++ src/app/(main)/tours/page.tsx
@@ -2,12 +2,14 @@ import { Pages } from '@/@types';
 import Gallery from '@/sections/Gallery';
 import Hero from '@/sections/Hero';
 import Reviews from '@/sections/Reviews';
+import Tips from '@/sections/Tips';
 
 const ToursPage = () => {
   return (
     <>
       <Hero page={Pages.TOURS} />
       <Gallery />
+      <Tips />
       <Reviews />
     </>
   );
