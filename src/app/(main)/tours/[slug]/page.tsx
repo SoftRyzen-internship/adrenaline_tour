f42c76b467ea2cac640ef8f68906a059215d80ee
Daryna Karmazin <daryna.karmazin@gmail.com>
2024-06-06 13:10:04 +0300

    Updated /[slug]/page.tsx

--- src/app/(main)/tours/[slug]/page.tsx
+++ src/app/(main)/tours/[slug]/page.tsx
@@ -1,5 +1,26 @@
 export interface ISingleTourPageProps {
   params: { id: string };
+  searchParams: { [key: string]: string | string[] | undefined };
+}
+
+import { Metadata } from 'next';
+
+const BASE_APP_URL = process.env.BASE_APP_URL as string;
+
+export async function generateMetadata({
+  params,
+}: ISingleTourPageProps): Promise<Metadata> {
+  const id = params.id;
+  const tour = await fetch(`${BASE_APP_URL}tours/${id}`).then(res =>
+    res.json(),
+  );
+
+  return {
+    title: tour.title,
+    alternates: {
+      canonical: params.id,
+    },
+  };
 }
 
 const SingleTourPage: React.FC<ISingleTourPageProps> = ({ params }) => {
