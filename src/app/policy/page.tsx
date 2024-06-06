18c8d11b49d2ad0a092704896ec1f8d12907feee
Daryna Karmazin <daryna.karmazin@gmail.com>
2024-06-06 16:06:11 +0300

    Tried request to strapi cms

--- src/app/policy/page.tsx
+++ src/app/policy/page.tsx
@@ -1,6 +1,44 @@
+'use client';
+
+import { useEffect } from 'react';
+
+import axios from 'axios';
+
 import Observer from '@/components/common/Observer';
 
+const API_URL = 'https://adrenaline-tour-admin.onrender.com/graphql';
+
+export const getPolicyData = async () => {
+  const query = `
+    query getPolicy {
+      policy {
+        data {
+          attributes {
+            title
+            text
+          }
+        }
+      }
+    }
+  `;
+  try {
+    const response = await axios.post(API_URL, { query });
+    return response.data.data.policy.data.attributes;
+  } catch (error) {
+    console.error('Error fetching policy data:', error);
+    return null;
+  }
+};
+
 const PolicyPage = () => {
+  useEffect(() => {
+    const fetchData = async () => {
+      const policyData = await getPolicyData();
+      console.log(policyData); // Виводить дані в консоль
+    };
+
+    fetchData();
+  }, []);
   return (
     <>
       <h1 className='text-6xl'>PolicyPage will be here</h1>
