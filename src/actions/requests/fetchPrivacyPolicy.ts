e10321f13fa5542fef8b348257d720468e5b6f6b
Daryna Karmazin <daryna.karmazin@gmail.com>
2024-06-08 09:48:22 +0300

    Added Policy section component

---
+++ src/actions/requests/fetchPrivacyPolicy.ts
@@ -0,0 +1,34 @@
+const API_URL = 'https://adrenaline-tour-admin.onrender.com/graphql';
+
+export const fetchPrivacyPolicy = async () => {
+  const query = `
+    query getPolicy {
+      policy {
+        data {
+          attributes {
+            title
+            text
+          }
+        }
+      }
+    }
+  `;
+
+  try {
+    const response = await fetch(API_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ query }),
+    });
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+    const result = await response.json();
+    return result.data.policy.data.attributes;
+  } catch (error) {
+    // console.error('Error fetching policy data:', error);
+    return null;
+  }
+};
--- src/sections/Policy/Policy.tsx
+++ src/sections/Policy/Policy.tsx
@@ -1,51 +1,18 @@
-'use client';
+import { PortableText } from '@portabletext/react';
 
-import { useEffect, useState } from 'react';
+import { fetchPrivacyPolicy } from '@/actions/requests/fetchPrivacyPolicy';
+import { CustomComponent } from '@/components/common/CustomComponent';
 
-import axios from 'axios';
+const Policy = async () => {
+  const policy = await fetchPrivacyPolicy();
 
-const API_URL = 'https://adrenaline-tour-admin.onrender.com/graphql';
-
-export const getPolicyData = async () => {
-  const query = `
-    query getPolicy {
-      policy {
-        data {
-          attributes {
-            title
-            text
-          }
-        }
-      }
-    }
-  `;
-  try {
-    const response = await axios.post(API_URL, { query });
-    return response.data.data.policy.data.attributes;
-  } catch (error) {
-    console.error('Error fetching policy data:', error);
-    return null;
-  }
-};
-
-const Policy = () => {
-  const [policy, setPolicy] = useState(null);
-  useEffect(() => {
-    const fetchData = async () => {
-      const policyData = await getPolicyData();
-      console.log(policyData);
-      setPolicy(policyData);
-    };
-
-    fetchData();
-  }, []);
   return (
     <section className='section'>
       <div className='container'>
         {policy && (
           <>
-            <h1>{policy.title}</h1>
-            <p>{policy.text}</p>
+            <h1 className=''>{policy.title}</h1>
+            <PortableText value={policy.text} components={CustomComponent} />
           </>
         )}
       </div>
