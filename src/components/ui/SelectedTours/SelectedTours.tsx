bfc41922ec2a5544ed80572bc52e62e7bf397c00
parasolya <o.shevchuk@gmail.com>
2024-06-13 13:02:58 +0300

    data for selects from getTours

--- src/components/ui/SelectedTours/SelectedTours.tsx
+++ src/components/ui/SelectedTours/SelectedTours.tsx
@@ -3,11 +3,7 @@
 import React, { useEffect, useState } from 'react';
 
 import { ISelectState } from '@/@types';
-import {
-  fetchAllTours,
-  fetchCountries,
-  fetchActivities,
-} from '@/actions/requests';
+import { fetchAllTours } from '@/actions/requests';
 
 import ArrowRightDownIcon from '/public/icons/arrow-right_up.svg';
 
@@ -16,6 +12,8 @@ import CustomSelect from '@/components/ui/CustomSelect';
 import LinkButton from '@/components/ui/LinkButton';
 import TourCard from '@/components/ui/TourCard';
 import { selectedTours } from '@/data';
+
+import ErrorComponent from '../ErrorComponent';
 
 import { ITour } from './SelectedTours.types';
 
@@ -52,59 +50,140 @@ const SelectedTours = () => {
     }
   };
 
-  const fetchData = async () => {
-    try {
-      const fetchedCountries = await fetchCountries();
-      const fetchedActivities = await fetchActivities();
+  // const fetchData = async () => {
+  //   try {
+  //     const fetchedCountries = await fetchCountries();
+  //     const fetchedActivities = await fetchActivities();
 
-      const countryNames = fetchedCountries.map(
-        country => country.attributes.name,
-      );
-      const updatedCountries = countryNames.map((name, index) => ({
-        id: index + 1,
-        attributes: { name },
-      }));
-      updatedCountries.unshift({ id: 0, attributes: { name: 'Всі країни' } });
-      setFilteredCountries(updatedCountries);
+  //     const countryNames = fetchedCountries.map(
+  //       country => country.attributes.name,
+  //     );
+  //     const updatedCountries = countryNames.map((name, index) => ({
+  //       id: index + 1,
+  //       attributes: { name },
+  //     }));
+  //     updatedCountries.unshift({ id: 0, attributes: { name: 'Всі країни' } });
+  //     setFilteredCountries(updatedCountries);
 
-      const activitieNames = fetchedActivities.map(
-        activity => activity.attributes.name,
-      );
-      const updatedActivities = activitieNames.map((name, index) => ({
-        id: index + 1,
-        attributes: { name },
-      }));
-      updatedActivities.unshift({
-        id: 0,
-        attributes: { name: 'Всі активності' },
-      });
-      setFilteredActivities(updatedActivities);
-    } catch (error) {
-      console.error('Error fetching data:', error);
-    }
-  };
-
-  const filterToursByActivity = async (activity: string) => {
-    const filteredTours = tours.filter(tour =>
-      tour.attributes.activities.data.some(
-        act => act.attributes.name === activity,
-      ),
-    );
-    setFilteredTours(filteredTours);
-  };
+  //     const activitieNames = fetchedActivities.map(
+  //       activity => activity.attributes.name,
+  //     );
+  //     const updatedActivities = activitieNames.map((name, index) => ({
+  //       id: index + 1,
+  //       attributes: { name },
+  //     }));
+  //     updatedActivities.unshift({
+  //       id: 0,
+  //       attributes: { name: 'Всі активності' },
+  //     });
+  //     setFilteredActivities(updatedActivities);
+  //   } catch (error) {
+  //     console.error('Error fetching data:', error);
+  //   }
+  // };
 
   useEffect(() => {
     fetchTheTours();
-    fetchData();
   }, []);
 
   useEffect(() => {
-    if (selectedActivity.attributes.name !== 'Всі активності') {
-      filterToursByActivity(selectedActivity.attributes.name);
+    setFilteredTours(filterTours(tours));
+    updateFilteredActivities();
+    updateFilteredCountries();
+  }, [selectedCountry, selectedActivity, tours]);
+
+  const updateFilteredActivities = () => {
+    let activities: string[] = [];
+
+    if (selectedCountry.attributes.name === 'Всі країни') {
+      activities = tours.flatMap(tour =>
+        tour.attributes.activities.data.map(
+          activity => activity.attributes.name,
+        ),
+      );
     } else {
-      fetchTheTours();
+      const toursInSelectedCountry = tours.filter(tour =>
+        tour.attributes.countries.data.some(
+          country =>
+            country.attributes.name === selectedCountry.attributes.name,
+        ),
+      );
+      activities = toursInSelectedCountry.flatMap(tour =>
+        tour.attributes.activities.data.map(
+          activity => activity.attributes.name,
+        ),
+      );
     }
-  }, [selectedActivity]);
+
+    const uniqueActivities = Array.from(new Set(activities)).sort((a, b) =>
+      a.localeCompare(b),
+    );
+    const updatedActivities = uniqueActivities.map((name, index) => ({
+      id: index + 1,
+      attributes: { name },
+    }));
+
+    updatedActivities.unshift({
+      id: 0,
+      attributes: { name: 'Всі активності' },
+    });
+
+    setFilteredActivities(updatedActivities);
+  };
+
+  const updateFilteredCountries = () => {
+    let countries: string[] = [];
+
+    if (selectedActivity.attributes.name === 'Всі активності') {
+      countries = tours.flatMap(tour =>
+        tour.attributes.countries.data.map(country => country.attributes.name),
+      );
+    } else {
+      const toursWithSelectedActivity = tours.filter(tour =>
+        tour.attributes.activities.data.some(
+          activity =>
+            activity.attributes.name === selectedActivity.attributes.name,
+        ),
+      );
+      countries = toursWithSelectedActivity.flatMap(tour =>
+        tour.attributes.countries.data.map(country => country.attributes.name),
+      );
+    }
+
+    const uniqueCountries = Array.from(new Set(countries)).sort((a, b) =>
+      a.localeCompare(b),
+    );
+    const updatedCountries = uniqueCountries.map((name, index) => ({
+      id: index + 1,
+      attributes: { name },
+    }));
+
+    updatedCountries.unshift({ id: 0, attributes: { name: 'Всі країни' } });
+
+    setFilteredCountries(updatedCountries);
+  };
+
+  const filterTours = (tours: ITour[]) => {
+    let newFilteredTours = [...tours];
+
+    if (selectedCountry.attributes.name !== 'Всі країни') {
+      newFilteredTours = newFilteredTours.filter(tour =>
+        tour.attributes.countries.data
+          .map(country => country.attributes.name)
+          .includes(selectedCountry.attributes.name),
+      );
+    }
+
+    if (selectedActivity.attributes.name !== 'Всі активності') {
+      newFilteredTours = newFilteredTours.filter(tour =>
+        tour.attributes.activities.data
+          .map(activity => activity.attributes.name)
+          .includes(selectedActivity.attributes.name),
+      );
+    }
+
+    return newFilteredTours;
+  };
 
   const loadMore = () => {
     setVisibleTours(prev => prev + 6);
@@ -128,14 +207,17 @@ const SelectedTours = () => {
           onChange={(item: ISelectState) => setSelectedCountry(item)}
         />
       </div>
-
-      <div className='my-10 grid gap-[32px] md:my-14 md:grid-cols-2 md:gap-x-[32px] md:gap-y-[40px] xl:my-16 xl:grid-cols-3'>
-        {filteredTours.slice(0, visibleTours).map((tour, index) => (
-          <div key={index} className='col-span-1'>
-            <TourCard data={tour} />
-          </div>
-        ))}
-      </div>
+      {filteredTours.length > 0 ? (
+        <div className='my-10 grid gap-[32px] md:my-14 md:grid-cols-2 md:gap-x-[32px] md:gap-y-[40px] xl:my-16 xl:grid-cols-3'>
+          {filteredTours.slice(0, visibleTours).map((tour, index) => (
+            <div key={index} className='col-span-1'>
+              <TourCard data={tour} />
+            </div>
+          ))}
+        </div>
+      ) : (
+        <ErrorComponent />
+      )}
 
       <div className='flex justify-center'>
         {filteredTours.length > 6 &&
