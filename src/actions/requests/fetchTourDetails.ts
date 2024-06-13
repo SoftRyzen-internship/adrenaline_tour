9ce83b3b3dec1f013095f491855be805ad67e2e0
Oleg <ikslprint@gmail.com>
2024-06-13 17:10:52 +0300

    remove useState, useEffect

--- src/actions/requests/fetchTourDetails.ts
+++ src/actions/requests/fetchTourDetails.ts
@@ -4,10 +4,7 @@ import fetchData from '../fetchData';
 import { getTourDetails as query } from '../query';
 
 const fetchTourDetails = async (slug: string) => {
-  const variables = {
-    slug,
-  };
-  const result = (await fetchData(query, variables)) as ITourDetailsResponse;
+  const result = (await fetchData(query, { slug })) as ITourDetailsResponse;
 
   return result.tours.data;
 };
--- src/components/common/FeaturesTable/FeaturesTable.tsx
+++ src/components/common/FeaturesTable/FeaturesTable.tsx
@@ -1,9 +1,6 @@
-'use client';
-import React, { useState, useEffect } from 'react';
-
-import { ITourDetails } from '@/@types';
 import { IdForScroll } from '@/@types';
 import fetchTourDetails from '@/actions/requests/fetchTourDetails';
+import ErrorComponent from '@/components/ui/ErrorComponent';
 import FeatureRow from '@/components/ui/FeatureRow';
 import LinkButton from '@/components/ui/LinkButton';
 import Social from '@/components/ui/Social';
@@ -14,28 +11,21 @@ import { featuresData } from '@/data';
 
 import { IFeaturesTableProps } from './FeaturesTable.types';
 
-const FeaturesTable: React.FunctionComponent<IFeaturesTableProps> = ({
+const FeaturesTable: React.FunctionComponent<IFeaturesTableProps> = async ({
   slug,
 }) => {
-  const [tourFeatures, setTourFeatures] = useState<ITourDetails | null>(null);
   const { buttonLabel } = featuresData;
 
-  console.log(tourFeatures);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const tourDetails = await fetchTourDetails(slug);
-      setTourFeatures(tourDetails[0]);
-    };
-
-    fetchData();
-  }, [slug]);
+  const tourDetails = await fetchTourDetails(slug);
+  if (!tourDetails) {
+    return <ErrorComponent isLoadingError />;
+  }
 
   return (
     <div className='border-grey03 border-2 p-6 md:py-8 xl:max-w-[500px]'>
       <ul className='mb-8 flex flex-col gap-6 xl:mb-12'>
-        {tourFeatures &&
-          Object.entries(tourFeatures.attributes.details).map(
+        {tourDetails &&
+          Object.entries(tourDetails[0].attributes.details).map(
             ([key, value]) => (
               <li key={key}>
                 <FeatureRow feature={{ icon: key, details: value }} />
