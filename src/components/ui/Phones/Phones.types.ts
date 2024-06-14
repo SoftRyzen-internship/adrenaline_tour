5e85e12113c03f4841d34b50a648b26019f22591
DisRachik <radchenkoig@gmail.com>
2024-06-14 22:51:57 +0300

    change Phones from client component to server

--- src/components/ui/Phones/Phones.types.ts
+++ src/components/ui/Phones/Phones.types.ts
@@ -2,20 +2,3 @@ export interface IPhonesProps {
   variant: 'contacts' | 'footer';
   className?: string;
 }
-
-export interface IContact {
-  email: string;
-  numbers: {
-    [country: string]: string;
-  };
-}
-
-interface ContactData {
-  attributes: IContact;
-}
-
-export interface IContactResponse {
-  contact: {
-    data: ContactData;
-  };
-}
