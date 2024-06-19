952f8923790eb9222329958ac3724223828063d4
TatianaSkl <sklyarchyk@gmail.com>
2024-06-19 12:55:07 +0300

    added LoadingComponent

--- src/app/(main)/tours/[slug]/loading.tsx
+++ src/app/(main)/tours/[slug]/loading.tsx
@@ -1,26 +1,7 @@
-'use client';
-
-import { CSSProperties } from 'react';
-
-import BarLoader from 'react-spinners/BarLoader';
-
-const override: CSSProperties = {
-  display: 'block',
-  margin: '0 auto',
-};
+import LoadingComponent from '@/components/ui/LoadingComponent';
 
 const Loading = () => {
-  return (
-    <div className='ml-8 p-8'>
-      <BarLoader
-        color='#FF4F1E'
-        height={8}
-        width='100%'
-        cssOverride={override}
-        loading
-      />
-    </div>
-  );
+  return <LoadingComponent />;
 };
 
 export default Loading;
